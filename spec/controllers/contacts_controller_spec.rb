4d7e3480e811957f99d5bea0184c91fa5e319d01
David Peña <dpena@grupoecce.com>
2020-03-08 13:17:28 -0600

    re factor testing for controllers

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -2,17 +2,24 @@ require 'rails_helper'
 
 describe ContactsController do
     describe '#index' do
+        subject { get :index }
         it 'should return a successs response' do
-            get :index
+            subject
             expect(response).to have_http_status(:ok)
         end
 
         it 'should return proper json' do
-            create_list :contact, 2 
-            get :index
-            json = JSON.parse(response.body)
-            json_data = json['data']
-            expect(json_data.length).to eq(2)
+            contacts = create_list :contact, 2 
+            subject           
+            contacts.each_with_index do |contact, i|
+                expect(json_data[i]['attributes']).to eq({
+                    "email" => contact.email,
+                    "first-name" => contact.first_name,
+                    "last-name" => contact.last_name,          
+                    "phone-number" => contact.phone_number
+                })
+            end
         end
     end
 end
+
--- spec/factories/contacts.rb
+++ spec/factories/contacts.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     sequence(:first_name) {|n| "My first name #{n}"}
     sequence(:last_name) {|n| "my last name #{n}"}
     sequence(:email) {|n| "myemail#{n}@example.com"}
-    sequence(:phone_number) {|n| ("#{n}29612255035".to_i) }
+    sequence(:phone_number) {|n| "#{n}29612255035" }
 
   end
 end
