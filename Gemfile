9b54fedeff1ea07ddac7f5580474df0c7cd9e529
David Peña <dpena@grupoecce.com>
2020-03-11 20:17:51 -0600

    working change seed, and gitname change

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'factory_bot_rails'
 gem 'active_model_serializers'
 gem 'rspec-rails'
 gem 'kaminari'
+gem 'rack-cors'
 
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 # gem 'rack-cors'
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Contact.create(firstname: 'Jorge Fernando1', lastname: 'Rincon Figuero1a', email: 'myemail@examp1le.com', phonenumber: '521612255035')
+Contact.create(firstname: 'Jorg2e Fernando', lastname: 'Rinco2n Figueroa', email: 'myemail@2example.com', phonenumber: '522612255035')
+Contact.create(firstname: 'Jorge3 Fernando', lastname: 'Rincon3 Figueroa', email: 'myem3il@example.com', phonenumber: '329612255035')
+Contact.create(firstname: 'Jorg4e Fernando', lastname: 'Ri4ncon Figueroa', email: 'myema4il@example.com', phonenumber: '549612255035')
+Contact.create(firstname: 'Jorge Fe5rnando', lastname: 'Rinc5on Figueroa', email: 'myema5il@example.com', phonenumber: '525612255035')
+Contact.create(firstname: 'Jorge Fer6nando', lastname: 'Rinc6on Figueroa', email: 'mye6mail@example.com', phonenumber: '526612255035')
+Contact.create(firstname: 'Jor7ge Fernando', lastname: 'Rinco7n Figueroa', email: 'myem7ail@example.com', phonenumber: '579612255035')
+Contact.create(firstname: 'Jorge 8Fernando', lastname: 'Rinco8n Figueroa', email: 'myema8il@example.com', phonenumber: '589612255035')
+Contact.create(firstname: 'Jorg9e Fernando', lastname: 'Rinc9on Figueroa', email: 'myemai9l@example.com', phonenumber: '929612255035')
+Contact.create(firstname: 'Jorge F0ernando', lastname: 'Rin0con Figueroa', email: 'myema0il@example.com', phonenumber: '529610255035')
